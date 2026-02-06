ea3270a38c43d0509ecb58c5db38661e5181a0dc
FelpHendeson <felipe.hendeson@fan-edu.com.br>
2026-02-06 13:40:39 -0300

    feat: Configração do Vite para expor projeto no ip e definir porta

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // escuta em 0.0.0.0 (acessível pelo IP da máquina)
+    port: 5173,
+    strictPort: true,
+  },
 });
